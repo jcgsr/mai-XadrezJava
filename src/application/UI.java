9fc55716fa5272395a2d184e9f96ccc946d3c142
jcgsr <cabralguerra@gmail.com>
2022-05-07 05:15:35 -0300

    checkmate

--- src/application/UI.java
+++ src/application/UI.java
@@ -47,15 +47,20 @@ public class UI {
     }
   }
 
-  public static void printMatch(ChessMatch match, List<ChessPiece> captured) {
-    printBoard(match.getPieces());
+  public static void printMatch(ChessMatch chessMatch, List<ChessPiece> captured) {
+    printBoard(chessMatch.getPieces());
     System.out.println();
     printCapturedPieces(captured);
     System.out.println();
-    System.out.println("Turn: " + match.getTurn());
-    System.out.println("Waiting for: " + match.getCurrentPlayer());
-    if (match.getCheck()) {
-      System.out.println("CHECK!");
+    System.out.println("Turn : " + chessMatch.getTurn());
+    if (!chessMatch.getCheckMate()) {
+      System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+      if (chessMatch.getCheck()) {
+        System.out.println("CHECK!");
+      }
+    } else {
+      System.out.println("CHECKMATE!");
+      System.out.println("Winner: " + chessMatch.getCurrentPlayer());
     }
   }
 
